b62cb4358c811cd68f064b4256af9630107b03b2
hemanth18 <hg510443@gmail.com>
2025-01-05 21:00:22 +0530

    Primary backend code for handling user's and zap's related operations

---
+++ primary_backend/src/index.ts
@@ -0,0 +1,17 @@
+// primary http server to create zap and other bunch of things
+import express from "express"
+import { userRouter } from "./router/user";
+import { zapRouter } from "./router/zap";
+import cors from "cors"
+
+const app = express();
+
+app.use(express.json())
+
+app.use(cors())
+
+app.use('/api/v1/user', userRouter);
+
+app.use('/api/v1/zap', zapRouter);
+
+app.listen(3001)
---
+++ primary_backend/src/router/user.ts
@@ -0,0 +1,100 @@
+import { Router, Request, Response } from "express";
+import express from "express"
+import { authMiddleware } from "../middleware";
+import { SigninSchema, SignupSchema } from "../types";
+import { prismaClient } from "../db";
+import jwt from "jsonwebtoken";
+import { JWT_PASSWORD } from "../config";
+
+const router = Router();
+router.use(express.json())
+
+router.post("/signup", async (req: any, res: any) => {
+  
+  const body = req.body
+  const parsedData = SignupSchema.safeParse(body);
+
+  if(!parsedData.success) {
+    return res.status(411).json({
+      message: "Incorrect inputs"
+    })
+  }
+
+  const userExists = await prismaClient.user.findFirst({
+    where: {
+      email: parsedData.data.username
+    }
+  });
+
+  if (userExists) {
+    return res.status(403).json({
+      message: "User already exits"
+    })
+  }
+
+  await prismaClient.user.create({
+    data:{
+      email: parsedData.data.username,
+      password: parsedData.data.password,
+      name: parsedData.data.name
+    }
+  })
+
+  return res.status(200).json({
+    message: "Please verify your account by checking your email"
+  })
+
+})
+
+router.post("/signin", async(req: any, res: any) => {
+  const body = req.body;
+  const parsedData = SigninSchema.safeParse(body);
+
+  if(!parsedData.success) {
+    return res.status(411).json({
+      message: "Incorrect inputs"
+    })
+  }
+
+  const user = await prismaClient.user.findFirst({
+    where: {
+      email: parsedData.data.username,
+      password: parsedData.data.password
+    }
+  });
+
+  if(!user) {
+    return res.status(403).json({
+      message: "Sorry crdentials are incorrect"
+    })
+  }
+
+  const token = jwt.sign({
+    id: user.id
+  }, JWT_PASSWORD)
+
+  res.json({
+    token: token
+  })
+})
+
+router.get("/", authMiddleware, async(req: any, res: any) => {
+  // fix the type here
+  // @ts-ignore
+  const id = req.id
+  const user = await prismaClient.user.findFirst({
+    where: {
+      id
+    },
+    select:{
+      name: true,
+      email: true
+    }
+  })
+
+  return res.json({
+    user
+  })
+})
+
+export const userRouter = router;
