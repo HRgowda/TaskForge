b8496a1b28ad10df8a40f2052449173f56bcac3e
hemanth18 <hg510443@gmail.com>
2025-01-31 05:35:27 +0530

    fix: Include seed.ts in typescript compilation by adjusting rootDir

---
+++ primary_backend/prisma/seed.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@prisma/client";
+const prismaClient = new PrismaClient()
+
+async function main() {
+  await prismaClient.availableTrigger.create({
+    data: {
+      id: "webhook",
+      name: "Webhook",
+      image: "https://th.bing.com/th/id/OIP.2Y0rBmvsTvZ0l7vvmiCHtgHaG5?w=189&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+    }
+  });
+
+  await prismaClient.availableAction.create({
+    data: {
+      id: "Email",
+      name: "Email",
+      image: "https://th.bing.com/th/id/R.229079c8f5240851cece598cf8eee770?rik=JND2PKmC%2fxzB1w&riu=http%3a%2f%2fpngimg.com%2fuploads%2femail%2femail_PNG11.png&ehk=6sNwAjueFilXp3tCehLPbXDGgZgsYZdR7y6dZ3vpSk4%3d&risl=&pid=ImgRaw&r=0"
+    }
+  });
+
+  await prismaClient.availableAction.create({
+    data: {
+      id: "Solana",
+      name: "Send_Solana",
+      image: "https://th.bing.com/th?q=Solana+Coin+PNG&w=120&h=120&c=1&rs=1&qlt=90&cb=1&dpr=1.3&pid=InlineBlock&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247"
+    }
+  })
+}
+
+main()
