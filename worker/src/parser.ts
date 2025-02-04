cd181dc579d0dcab82642db7c618774dcee6e1fa
hemanth18 <hg510443@gmail.com>
2025-02-05 03:28:32 +0530

    fix parser function

--- worker/src/parser.ts
+++ worker/src/parser.ts
@@ -1,56 +1,57 @@
 // function to extract the string value present inside the placeHolder.
 
 export function parse(text: string, value: any, startDelimeter = "{", endDelimeter = "}") {
-
+  console.log(`Parsing text: ${text}`);
+  
   let startIndex = 0
   let endIndex = 1
 
   let finalString = ""
 
-  while(endIndex < text.length) {
+  while (endIndex < text.length) {
+    
     // logic to process if we got the startDelimeter. Find the endDelimeter.
-    if (text[startIndex] === startDelimeter){
-      let endPoint = startIndex + 2 // +2 because to skip 1 iteration .
-      while(text[endPoint] !== endDelimeter){
-        endPoint ++
+    if (text[startIndex] === startDelimeter) {
+      let endPoint = startIndex + 2; // +2 because to skip 1 iteration .
+      while (text[endPoint] !== endDelimeter) {
+        endPoint++;
       }
-      // extract the values present in the placeholder i.e {comment.amount} for example in this case.
+
+      // Extract the placeholder, e.g. {comment.amount}
       let stringHoldingValue = text.slice(startIndex + 1, endPoint);
 
-      // split it into an array of the stringValue
+      // Split the placeholder into keys (e.g. ["comment", "amount"])
       let keys = stringHoldingValue.split(".");
-      
-      // we are soring it in localValues because we dont need to modify the original value.
-      let localValues = {
-        ...value
-      }
 
+      // we are storing it in localValues because we dont need to modify the original value.
+      let localValues = { ...value };
 
-      for(let i = 0; i < keys.length; i++){
-        if (typeof localValues === "string"){
-          localValues = JSON.parse(localValues)
+      for (let i = 0; i < keys.length; i++) {
+        if (typeof localValues === "string") {
+          localValues = JSON.parse(localValues);
         }
-        localValues = localValues[keys[i]]
+        localValues = localValues[keys[i]];
       }
 
       finalString += localValues;
       startIndex = endPoint + 1;
-      endIndex = endPoint + 2
-
+      endIndex = endPoint + 2;
     } else {
-      finalString += text[startIndex]
-      startIndex ++ 
-      endIndex ++
+      finalString += text[startIndex];
+      startIndex++;
+      endIndex++;
     }
   }
 
   // Edge case for last string in the text
-  if(text[startIndex]) {
-    finalString += text[startIndex]
+  if (text[startIndex]) {
+    finalString += text[startIndex];
   }
+
   return finalString;
 }
 
+// Example input
 // let value = parse("You received {comment.amount} money from {comment.address}", 
 //   {
 //   comment: {
