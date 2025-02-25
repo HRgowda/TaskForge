1154be8449d71d68cd741c09d457df02b67cdf1d
hemanth18 <hg510443@gmail.com>
2025-02-25 21:09:07 +0530

    Updated Selectors

--- frontend/components/Input.tsx
+++ frontend/components/Input.tsx
@@ -1,15 +1,22 @@
 "use client";
 
-export const Input = ({label, placeholder, onChange, type = "text"}: {
-    label: string;
-    placeholder: string;
-    onChange: (e: any) => void;
-    type?: "text" | "password"
+export const Input = ({ label, placeholder, onChange, type = "text" }: { 
+  label: string;
+  placeholder: string;
+  onChange: (e: any) => void;
+  type?: "text" | "password";
 }) => {
-    return <div className="w-full px-4 py-3 border border-slate-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all">
-        <div className="text-sm pb-1 pt-2">
-            * <label>{label}</label>
-        </div>
-        <input className="border rounded px-4 py-2 w-full border-slate-500 bg-[#1c1c1c]" type={type} placeholder={placeholder} onChange={onChange} />
+  return (
+    <div className="w-full">
+      <div className="text-sm pb-1 pt-2">
+        * <label>{label}</label>
+      </div>
+      <input 
+        className="w-full sm:min-w-[20rem] md:min-w-[24rem] lg:min-w-[28rem] px-4 py-3 border border-slate-500 rounded-lg bg-[#1c1c1c] text-white focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all"
+        type={type} 
+        placeholder={placeholder} 
+        onChange={onChange} 
+      />
     </div>
-}   
+  );
+};
