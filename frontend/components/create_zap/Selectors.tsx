959135e580e024f4b87c86983c26937a3442aef3
hemanth18 <hg510443@gmail.com>
2025-02-25 19:11:29 +0530

    Refactored Selctor_Modal input for better usability

--- frontend/components/create_zap/Selectors.tsx
+++ frontend/components/create_zap/Selectors.tsx
@@ -11,10 +11,10 @@ export function EmailSelector({ setMetadata }: { setMetadata: (params: any) => v
   return (
     <div className="text-white space-y-6 p-6 bg-[#1c1c1c] rounded-lg ">
       <div>
-        <Input label="To" placeholder="Enter recipient's email" onChange={(e: any) => setEmail(e.target.value)} />
+        <Input label="To" placeholder="Enter recipient's email as {comment.email}" onChange={(e: any) => setEmail(e.target.value)} />
       </div>
       <div>
-        <Input label="Body" type="text" placeholder="Enter email body" onChange={(e: any) => setBody(e.target.value)} />
+        <Input label="Body" type="text" placeholder="Write your email content here..." onChange={(e: any) => setBody(e.target.value)} />
       </div>
       <div className="pt-4">
         <Button onClick={() => { setMetadata({email, body}) }}className="w-full py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-300" >
@@ -32,10 +32,10 @@ export function SolanaSelector({ setMetadata }: { setMetadata: (params: any) => 
   return (
     <div className="text-white space-y-6 p-6 bg-[#1c1c1c] rounded-lg">
       <div>
-        <Input label="To" type="text" placeholder="Enter email" onChange={(e: any) => setAddress(e.target.value)} />
+        <Input label="To" type="text" placeholder="Enter recipient's email as {comment.email}" onChange={(e: any) => setAddress(e.target.value)} />
       </div>
       <div>
-        <Input label="Solana" type="text" placeholder="Enter the number SOL USDC" onChange={(e: any) => setAmount(e.target.value)} />
+        <Input label="Solana" type="text" placeholder="Enter the number SOLANA USDC as {comment.amount}" onChange={(e: any) => setAmount(e.target.value)} />
       </div>
       <div className="pt-4">
         <Button onClick={() => { setMetadata({amount, address}) }}className="w-full py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-300" >
@@ -53,10 +53,10 @@ export function InrSelector ({ setMetadata }: { setMetadata: (params: any) => vo
   
   return <div className="text-white space-y-6 p-6 rounded-lg bg-[#1c1c1c] shadow-md">
     <div>
-      <Input label="To" placeholder="Enter address" type="text" onChange={(e: any) => setAddress(e.target.value)}></Input>
+      <Input label="To" placeholder="Enter recipient's email as {comment.email}" type="text" onChange={(e: any) => setAddress(e.target.value)}></Input>
     </div>
     <div>
-      <Input label="Amount" placeholder="Enter the amount" type="text" onChange={(e: any) => setAmount(e.target.value)}></Input>
+      <Input label="Amount" placeholder="Enter the INR as {comment.amount}" type="text" onChange={(e: any) => setAmount(e.target.value)}></Input>
     </div>
     <div className="pt-4">
       <Button onClick={() => {
